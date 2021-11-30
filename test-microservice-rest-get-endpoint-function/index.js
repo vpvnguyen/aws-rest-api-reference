8cb57d7b4fa302a0b9ac5a4cea4e9200b1cbc1c1
vpvnguyen <vpvnguyen@gmail.com>
2021-11-29 18:13:49 -0800

    initial test function

---
+++ test-microservice-rest-get-endpoint-function/index.js
@@ -0,0 +1,53 @@
+const AWS = require("aws-sdk");
+
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+/**
+ * Demonstrates a simple HTTP endpoint using API Gateway. You have full
+ * access to the request and response payload, including headers and
+ * status code.
+ *
+ * To scan a DynamoDB table, make a GET request with the TableName as a
+ * query string parameter. To put, update, or delete an item, make a POST,
+ * PUT, or DELETE request respectively, passing in the payload to the
+ * DynamoDB API as a JSON body.
+ */
+exports.handler = async (event, context) => {
+  //console.log('Received event:', JSON.stringify(event, null, 2));
+
+  let body;
+  let statusCode = "200";
+  const headers = {
+    "Content-Type": "application/json",
+  };
+
+  try {
+    switch (event.httpMethod) {
+      case "DELETE":
+        body = "DELETE SUCCESS";
+        break;
+      case "GET":
+        body = "GET SUCCESS";
+        break;
+      case "POST":
+        body = "POST SUCCESS";
+        break;
+      case "PUT":
+        body = "PUT SUCCESS";
+        break;
+      default:
+        throw new Error(`Unsupported method "${event.httpMethod}"`);
+    }
+  } catch (err) {
+    statusCode = "400";
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
+  }
+
+  return {
+    statusCode,
+    body,
+    headers,
+  };
+};
